2d88e6211ab10688f897759105a4ee0c37fb402d
gooch lapeyronie <goochlapeyronie@gmail.com>
2022-04-01 16:08:04 +0200

    controller up and tested with postman

--- src/resources/dinosaurs/dinosaur.controller.ts
+++ src/resources/dinosaurs/dinosaur.controller.ts
@@ -1,41 +1,96 @@
-import { RequestHandler } from 'express';
+import { Response, Request, NextFunction } from 'express';
+import Dinosaur from './dinosaur.model';
 
-export const getDinosaurs: RequestHandler = async (
-  req,
-  res,
-  next
+export const getDinosaurs = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response | void> => {
-  res.status(200).json({ success: true, data: 'get all dinosaurs' });
+  try {
+    const dinosaurs = await Dinosaur.find();
+
+    res.status(200).json({ success: true, data: dinosaurs });
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
 };
 
-export const getDinosaur: RequestHandler = async (
-  req,
-  res,
-  next
+export const getDinosaur = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response | void> => {
-  res.status(200).json({ success: true, data: 'get one dinosaur' });
+  try {
+    const dinosaur = await Dinosaur.findById(req.params.id);
+
+    if (!dinosaur) {
+      return res.status(404).json({
+        success: false,
+        message: `dinosaur with id ${req.params.id} not found`,
+      });
+    }
+
+    res.status(200).json({ success: true, data: dinosaur });
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
 };
 
-export const createDinosaur: RequestHandler = async (
-  req,
-  res,
-  next
+export const createDinosaur = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response | void> => {
-  res.status(200).json({ success: true, data: 'create dinosaur' });
+  try {
+    const dinosaur = await Dinosaur.create(req.body);
+
+    res.status(201).json({ success: true, data: dinosaur });
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
 };
 
-export const updateDinosaur: RequestHandler = async (
-  req,
-  res,
-  next
+export const updateDinosaur = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response | void> => {
-  res.status(200).json({ success: true, data: 'update dinosaur' });
+  try {
+    const dinosaur = await Dinosaur.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!dinosaur) {
+      return res.status(404).json({
+        success: false,
+        message: `dinosaur with id ${req.params.id} not found`,
+      });
+    }
+
+    res.status(200).json({ success: true, data: dinosaur });
+  } catch (error) {
+    return res.status(400).json({ success: false, error });
+  }
 };
 
-export const deleteDinosaur: RequestHandler = async (
-  req,
-  res,
-  next
+export const deleteDinosaur = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<Response | void> => {
-  res.status(200).json({ success: true, data: 'delete a dinosaur' });
+  try {
+    const dinosaur = await Dinosaur.findByIdAndDelete(req.params.id);
+
+    if (!dinosaur) {
+      return res.status(404).json({
+        success: false,
+        message: `dinosaur with id ${req.params.id} not found`,
+      });
+    }
+
+    res.status(204);
+  } catch (error) {
+    return res.status(400).json({ success: false, error });
+  }
 };
