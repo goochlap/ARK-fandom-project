92ac2a095599e4675bee130d150ad36255f0ae65
goochlap <goochlapeyronie@gmail.com>
2022-04-22 09:50:39 +0200

    navlanding & navbar added

---
+++ app/src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <div>
+      <router-link to="/"><img src="../assets/images/ark-logo.png" alt="ark-logo" /></router-link>
+      <h1>ARK Resources</h1>
+    </div>
+
+    <NavLanding />
+  </header>
+</template>
+
+<script>
+import NavLanding from './NavLanding.vue'
+
+export default {
+  name: 'NavBar',
+  components: {
+    NavLanding
+  }
+}
+</script>
+
+<style scoped lang="scss">
+header {
+  background-color: $black;
+  box-shadow: $box-shadow-dark;
+  color: $white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  position: fixed;
+  transition: 0.5s ease all;
+  width: 100%;
+
+  div {
+    display: flex;
+    align-items: center;
+    flex: 1;
+
+    h1 {
+      margin: auto;
+    }
+
+    img {
+      height: 4rem;
+      width: 6rem;
+    }
+  }
+}
+</style>
---
+++ app/src/components/NavLanding.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavLanding'
+}
+</script>
+
+<style scoped lang="scss">
+nav {
+  display: flex;
+  justify-content: center;
+
+  a {
+    align-items: center;
+    border-bottom: 1px solid transparent;
+    color: $white;
+    display: flex;
+    padding: 0.5rem 1rem;
+    text-decoration: none;
+
+    &:hover {
+      color: $secondary;
+      border-color: $secondary;
+      transition: 0.5s ease all;
+    }
+  }
+}
+</style>
