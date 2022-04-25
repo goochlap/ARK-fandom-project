45af38b8dadbec7b8abe42cc239cdc8d4c53acc7
gooch lapeyronie <goochlapeyronie@gmail.com>
2022-04-25 18:29:37 +0200

    create grid template on global app

--- app/vue.config.js
+++ app/vue.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
       sass: {
         additionalData: `
           @import "@/assets/scss/_variables.scss";
+          @import "@/assets/scss/global.scss";
         `
       }
     }
